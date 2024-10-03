5e679381023208252a2b203f8630342aed768653
Lily <blanchar2@grinnell.edu>
2024-10-03 09:30:29 -0500

    Implement the  method.

--- src/main/java/edu/grinnell/csc207/util/StringUtils.java
+++ src/main/java/edu/grinnell/csc207/util/StringUtils.java
@@ -9,7 +9,9 @@ import java.io.PrintWriter;
  * Assorted utilities for working with strings.
  * 
  * @author Samuel A. Rebelsky
- * @author Your Name Here
+ * @author Lily Blanchard
+ * @author Tiffany Yan
+ * @author Moise Milenge
  */ 
 public class StringUtils {
   // +------------------+--------------------------------------------
@@ -19,9 +21,34 @@ public class StringUtils {
   /**
    * Determine whether the parens match in string.
    */
-  public static boolean checkMatching(String str) {
+  public static boolean checkMatching(String str) throws Exception {
     Stack<Character> parens = new LinkedStack<Character>();
-    return false;       // STUB
+    for (int i = 0; i < str.length(); i++) {
+      if (str.charAt(i) != '(' && str.charAt(i) != ')'
+          && str.charAt(i) != '[' && str.charAt(i) != ']') {
+            continue;
+          } // if
+      if (str.charAt(i) == '(' || str.charAt(i) == '[') {
+        try{
+          parens.push(str.charAt(i));
+        } catch (Exception e) {
+          e.printStackTrace();
+        } // try/catch
+      } else if ((str.charAt(i) == ')' || str.charAt(i) == ']')
+            && parens.isEmpty()) {
+        throw new Exception("Mismatch");
+      } else if (str.charAt(i) == ')' && (parens.peek() == '(' )) {
+        parens.pop();
+      } else if (str.charAt(i) == ']' && (parens.peek() == '[' )) {
+        parens.pop();
+      } else {
+        throw new Exception("no matching paren/bracket");
+      } // if
+    } // for
+    if (!parens.isEmpty()) {
+      throw new Exception("Mismatch");
+    } // if
+    return true;      
   } // checkMatching
 } // class StringUtils    
 
